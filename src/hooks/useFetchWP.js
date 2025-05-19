2e80c0b3af6bc53f1fb052513cbd298329ebd0ac
Eduardo Sanchez <laloptk@users.noreply.github.com>
2025-05-19 14:28:45 -0600

    Add the possibility to add query parameters from the hook

--- src/hooks/useFetchWP.js
+++ src/hooks/useFetchWP.js
@@ -1,7 +1,7 @@
 import FetchWP from "../utils/FetchWP"
 import { useEffect, useRef, useState } from "react"
 
-const useFetchWP = (type = 'posts') => {
+const useFetchWP = (type = 'posts', params = {}) => {
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
@@ -11,31 +11,40 @@ const useFetchWP = (type = 'posts') => {
         const fetchData = async () => {
             if (hasFetched.current) return;
             setLoading(true)
+            const defaultParams = {
+                per_page: 10,
+                page: 1,
+            }
+
+            const finalParams = {
+                ...defaultParams,
+                ...params
+            }
             
             try {
                 switch(type) {
                     case 'posts':
-                        const posts = await fetchWp.getPosts();
+                        const posts = await fetchWp.getPosts(finalParams);
                         setData(posts);
                         break;
                     case 'pages':
-                        const pages = await fetchWp.getPages();
+                        const pages = await fetchWp.getPages(finalParams);
                         setData(pages);
                         break;
                     case 'categories':
-                        const categories = await fetchWp.getCategories();
+                        const categories = await fetchWp.getCategories(finalParams);
                         setData(categories);
                         break;
                     case 'tags':
-                        const tags = await fetchWp.getTags();
+                        const tags = await fetchWp.getTags(finalParams);
                         setData(tags);
                         break;
                     case 'users':
-                        const users = await fetchWp.getUsers();
+                        const users = await fetchWp.getUsers(finalParams);
                         setData(users);
                         break;
                     case 'media':
-                        const media = await fetchWp.getMedia();
+                        const media = await fetchWp.getMedia(finalParams);
                         setData(media);
                         break;
                     default:
@@ -50,7 +59,6 @@ const useFetchWP = (type = 'posts') => {
             }
         }
         fetchData()
-        console.log(data)
     }, [type])
 
     return { data, loading, error }
