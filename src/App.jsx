3eb44cbe541769ede95621ec95cc0b73ebf6488a
Eduardo Sanchez <laloptk@users.noreply.github.com>
2025-05-19 18:43:57 -0600

    Clean up unnecessary files and code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import useFetchWP from './hooks/useFetchWP'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const {data, loading, error} = useFetchWP('posts')
-  if(data[0]) console.log(data[0]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-       
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div></div>
   )
 }
 
