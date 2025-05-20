17958562c2d77469fb7d33bac6f9e5ad511c031f
Eduardo Sanchez <laloptk@users.noreply.github.com>
2025-05-19 18:43:20 -0600

    Add Button component

---
+++ src/components/atoms/Button.jsx
@@ -0,0 +1,12 @@
+const Button = ({ children, onClick, className }) => {
+    return (
+        <button
+            className={`bg-blue-500 text-white font-bold py-2 px-4 rounded ${className}`}
+            onClick={onClick}
+        >
+            {children}          
+        </button>
+    )
+}
+
+export default Button
